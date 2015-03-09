308ecbe0788e105099f903965929b3ef5fbf6ead
Brody <info@jbbs-inc.com>
2015-03-09 09:02:39 -0700

    tweaks to .gitignore

---
+++ attributes/default.rb
@@ -0,0 +1,3 @@
+default['sdinc_jenkins']['file_cache_path'] = File.join(Chef::Config['file_cache_path'], 'sdinc_jenkins')
+
+default['sdinc_jenkins']['install_aptitude'] = 'yes'
---
+++ recipes/default.rb
@@ -0,0 +1,8 @@
+#
+#
+# Cookbook Name:: sdinc-jenkins
+# Recipe:: default
+#
+# Copyright (c) 2015 The Authors, All Rights Reserved.
+
+package 'aptitude'
