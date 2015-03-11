cbe681865aeaade1a653e8bcfc1dca2cbff35322
Brody <info@jbbs-inc.com>
2015-03-11 08:14:56 -0700

    jenkins up and running default

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,10 +1,11 @@
 TODO: Write features and bug fixes for each release. Example provided below:
 
+
+## Release 0.0.2
+
+Mainly wrapped the community jenkins cookbook
+
 ## Release 0.0.1
 
 Initial release of sdinc-jenkins
-
-* It makes a bacon sandwich
-* Also we fixed this bug, rejoice!
-
-[#25] A github style reference to github issue 25.
+Just the raw pan cookbook
--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,7 @@
 default['sdinc_jenkins']['file_cache_path'] = File.join(Chef::Config['file_cache_path'], 'sdinc_jenkins')
 
 default['sdinc_jenkins']['install_aptitude'] = 'yes'
-default['jenkins']['master']['install_method'] = 'war'
-default['jenkins']['master']['version'] = '1.601'
+default['jenkins']['master']['install_method'] = 'package'
+# TODO using war did not converge successfully
+# default['jenkins']['master']['install_method'] = 'war'
+# default['jenkins']['master']['version'] = '1.601'
